b28284bf1ccab049d0cb8ff86a0899ce6dab1d1d
Othman <othmanelallaoui2002@gmail.com>
2026-04-22 14:40:05 +0100

    version ahlam and time

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-        return ("Version ahlam elfadli test")
+        return f"Version ahlam elfadli test Heure: {datetime.now()}"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
