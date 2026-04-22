147678d7630478fc51f3e1f760cc235de608c256
Othman <othmanelallaoui2002@gmail.com>
2026-04-22 14:33:54 +0100

    test dynamique version6

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from datetime import datetime
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 4 - Nouvelle mise à jour"
+    return f"Version 6 - Heure: {datetime.now()}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+
+
+
+
+
+
