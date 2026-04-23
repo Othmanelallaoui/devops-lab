ba34f05c8ef8c62f1fdc57dd16ab40003df55166
Othman <othmanelallaoui2002@gmail.com>
2026-04-23 17:57:40 +0100

    version 14

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-        return f"Othman EL ALLAOUI Ahlam ELFADLI "
+        return f"Othman EL ALLAOUI Ahlam ELFADLi 2026"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
